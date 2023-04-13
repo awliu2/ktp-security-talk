a54ac641e1816592b089757b2bf5e74882427be0
Andi Liu <awliu2@uchicago.edu>
2023-04-13 16:34:39 -0500

    stuff

--- crib_drag.py
+++ crib_drag.py
@@ -3,10 +3,8 @@ import sys
 
 
 cipher_hex = [b'c2b8fd15d46e6008a97cc3644390b24a2cc784a7a4144dd504f431ec4a0456dfccdfb06d428d8016fc583526b73287deea40',
-			  b'ccabe40695025d15ec61d164729ae6563e8b9cb6e15378d31ffb3af8190558c3c993f358559c811df7143838ee53afc8e21a'
+				b'ccabe40695025d15ec61d164729ae6563e8b9cb6e15378d31ffb3af8190558c3c993f358559c811df7143838ee53afc8e21a'
 ]
-
-recovered = []
 
 
 cipher_bin = [binascii.unhexlify(c) for c in cipher_hex]
@@ -42,34 +40,21 @@ def byte_xor(a, b):
     return ([(ch_a ^ ch_b) for ch_a, ch_b in zip(a, b)])
 
 
-def verify():
-    key = (byte_xor(cipher_bin[2], recovered[2]))
-    # for i, p in enumerate(recovered):
-    #     key = (byte_xor(ctxts_bin[i], p))
-    #     print(key)
+def get_key(a, b):
+	return byte_xor(a, b)
 
-    for b in cipher_bin:
-        asciis = [byte for byte in (byte_xor(b, key))]
-        chars = [chr(byte) for byte in (byte_xor(b, key))]
-        print(asciis)
-        print(''.join(chars), end='')
-        
-
+# main function to handle command line arguments
 def main():
-    if ('-v') in sys.argv:
-            verify()
-            sys.argv.remove('-v')
-    if len(sys.argv) > 1:
-        arr1 = int(sys.argv[1])
-        arr2 = int(sys.argv[2])
-        crib = sys.argv[3]
-        if (not 0 <= arr1 <= 3) or (not 0 <= arr2 <= 3):
-            print('arrays out of range')
-            exit()
-        
-        crib_drag(byte_xor(cipher_bin[arr1], 
-                        cipher_bin[arr2]),
-                        crib)
+	if len(sys.argv) != 2:
+		print('Usage: python3 crib_drag.py <crib>')
+		print("Example: python3 crib_drag.py 'Eric'")
+		sys.exit(1)
+    
+	crib = sys.argv[1]
+	crib_drag(byte_xor(cipher_bin[0], 
+					cipher_bin[1]),
+					crib)
 
 if __name__ == '__main__':
+    # print(get_key(a, b))
     main()
